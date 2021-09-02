a2a7fe43b0e04a3fdb0d274ff7890132e8de7ba4
raja497 <83097824+raja497@users.noreply.github.com>
2021-09-03 00:55:16 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -118,7 +118,7 @@ async def play(_, message: Message):
                 try:
                     await USER.join_chat(invitelink)
                     await USER.send_message(
-                        message.chat.id, "**Mizuki Music assistant joined this group for play music 🎵**")
+                        message.chat.id, "**group Music assistant joined this group for play music 🎵**")
 
                 except UserAlreadyParticipant:
                     pass
@@ -143,7 +143,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/caeb50039026a746e7252.jpg"
+        thumb_name = "https://telegra.ph/file/cf19dda907391656338eb.png"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -152,8 +152,8 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="Channel 🔊",
-                        url="https://t.me/Infinity_BOTs")
+                        text="𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🔊",
+                        url="https://t.me/ABOUT_ABHINAS")
                    
                 ]
             ]
@@ -201,7 +201,7 @@ async def play(_, message: Message):
             )
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/638c20c44ca418c8b2178.jpg"
+            thumb_name = "https://telegra.ph/file/cf19dda907391656338eb.png"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
